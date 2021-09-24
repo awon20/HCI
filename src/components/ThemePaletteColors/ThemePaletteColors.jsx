0f0a4ecd0cc4ff37c5547ddb50d18e509fa346c8
Armel Wonga <Armel.Wonga@w-hs.de>
2021-09-24 07:35:21 +0200

    add a new Compo for the color in all Project

---
+++ src/components/ThemePaletteColors/ThemePaletteColors.jsx
@@ -0,0 +1,29 @@
+import { createTheme } from "@material-ui/core/styles";
+// eslint-disable-next-line no-lone-blocks
+{
+  /*This  component Providing the colors directly for the all App*/
+}
+export const ThemePaletteColors = createTheme({
+  palette: {
+    primary: {
+      light: "#688a80",
+      main: "#95C6B8",
+      dark: "#aad1c6",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#dc5d5d",
+      main: "#D43535",
+      dark: "#942525",
+      contrastText: "#000",
+    },
+    Text: {
+      light: "#F5F5F5",
+      main: "#ababab",
+      dark: "#333333",
+      contrastText: "#000",
+    },
+  },
+});
+
+
