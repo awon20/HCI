7cf1e5018cd14ed9725cc2ca3de930df472b1092
Armel Wonga <Armel.Wonga@w-hs.de>
2021-09-24 07:37:05 +0200

    remove the gestylled-Button and add MUI-Typo

--- src/components/RecordBoardButton/StartRecordBoardButton.jsx
+++ src/components/RecordBoardButton/StartRecordBoardButton.jsx
@@ -1,38 +1,87 @@
 import React from 'react';
 
-import { Box } from '@material-ui/core';
-import { RecordingAPI } from "../../components/";
+import { Box, Button, Typography} from '@material-ui/core';
+import { withStyles } from "@material-ui/core/styles";
+// import { BoardButton } from "../../components/";
 
-const buttonName = "Board Aufzeichnen";
-
-
+// const theme = createTheme();
 
 // const useStyles = makeStyles((theme) => ({
-//   margin: {
-//     position: "relative",
-//     top: 0,
+
+//   root: {
+//     // margin: theme.spacing(10),
+    
+//     mainColor: theme.palette.primary.main,
+//     backgroundColor: theme.palette.primary.dark,
+//     borderColor: theme.palette.primary.dark,
+//     // position: "relative",
+//     // bottom: 50,
 //     center: 0,
+//     "&:hover": {
+//       backgroundColor: theme.palette.primary.dark,
+//       borderColor: theme.palette.primary.dark,
+//       boxShadow: "none",
+//     },
 //   },
 // }));
+export const BoardButton = withStyles({
+  root: {
+    boxShadow: "none",
+    textTransform: "none",
+    fontSize: 24,
+    padding: "6px 16px",
+    border: "1px solid",
+    lineHeight: 1.5,
+    backgroundColor: "#aad1c6",
+    borderColor: "#aad1c6",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:hover": {
+      backgroundColor: "#95C6B8",
+      borderColor: "#95C6B8",
+      boxShadow: "none",
+    },
+  },
+})(Button);
 
-export function StartRecordBoardButton(props) {
+export function StartRecordBoardButton() {
+  //  const classes = useStyles();
   return (
     <div>
       <Box display="flex" justifyContent="center">
         {/* start Board recording */}
-        {/* <BoardButton
+        <BoardButton
           variant="contained"
           color="primary"
           disableRipple
           // getMediaStream={this.getMediaStream}
-          // className={classes.margin}
-        ></BoardButton> */}
+          // className={classes.root}
+        >
+          <Typography
+            variant="button"
+            component="h2"
+            align="center"
+            display="block"
+          >
+            Board Aufzeichnen
+          </Typography>
+        </BoardButton>
       </Box>
-      <div>
+      {/* <Box>
         <RecordingAPI>
-        {buttonName}
+        
         </RecordingAPI>
-      </div>
+      </Box> */}
     </div>
   );
 }
