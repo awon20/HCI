b5e8608581fc6156536a170f9fed8bf042b500b2
Armel Wonga <Armel.Wonga@w-hs.de>
2021-09-24 07:39:19 +0200

    add new MUI-Typo to this compo

--- src/components/CameraMicroBox/CameraMicroBox.jsx
+++ src/components/CameraMicroBox/CameraMicroBox.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Box } from "@material-ui/core";
-import { CameraLoader } from "../../components";
-import { MicroLoader } from "../../components";
-
+import { CameraLoader, MicroLoader } from "../../components";
 
 
 export function CameraMicroBox() {
 
     return (
-      // <Box display="flex" justifyContent="center" pt={4} pr={6} pb={4} pl={6}>
-      <Box display="flex" justifyContent="center">
-        {/* <Box m={1} p={1}> */}
-        <Box>
-          <CameraLoader />
+      <div>
+        <Box
+          display="flex"
+          justifyContent="center"
+          flexDirection="row"
+          alignItems="stretch"
+          padding={1}
+        >
+          <Box>
+            <CameraLoader />
+          </Box>
+          <Box m={1} p={1} />
+          <Box>
+            <MicroLoader />
+          </Box>
         </Box>
-        {/* <Box m={1} p={1}> */}
-        <Box>
-          <MicroLoader />
-        </Box>
-      </Box>
+      </div>
     );
 }
--- src/components/CardTitle/CardTitle.jsx
+++ src/components/CardTitle/CardTitle.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Typography from "@material-ui/core/Typography";
 
-const title = `Neues SketchBoard`;
-
 export  function CardTitle() {
   
     return (
-      <Typography component="h5" variant="h5">
-        {title}
+      <Typography 
+        variant="h5" 
+        component="h2" 
+        align="center" 
+        display="block"
+        color="textPrimary"
+        gutterBottom
+      >
+        Neues SketchBoard
       </Typography>
     );
 }
