39b2e2b84a4f98fd027843e142613704a207c3a9
Armel Wonga <Armel.Wonga@w-hs.de>
2021-09-24 07:34:19 +0200

    add a array of obj to call tools-icons

--- src/components/ToolsBox/ToolsBox.jsx
+++ src/components/ToolsBox/ToolsBox.jsx
@@ -1,4 +1,4 @@
-import { Divider, makeStyles } from '@material-ui/core';
+import { Divider, List, makeStyles, Box, ListItem, ListItemIcon, ListItemText  } from '@material-ui/core';
 import React from 'react';
 import { Icon } from "@iconify/react";
 import eraserIcon from "@iconify-icons/mdi/eraser";
@@ -10,73 +10,115 @@ import {
 } from "react-icons/md";
 import UndoIcon from "@material-ui/icons/Undo";
 import RedoIcon from "@material-ui/icons/Redo";
-import { Grid, Box }from "@material-ui/core";
+import { useHistory, useLocation } from 'react-router';
 
 
-
-// const useStyles = makeStyles((theme) => ({
-//   controlPosition: {
-//     display: "flex",
-//     alignItems: "center",
-//     padding: "0 30px",
-//   },
-//   sizeIcons: {
-//     height: "50px",
-//     width: "50px",
-//     marginRight: "20px",
-//     marginLeft: "20px",
-//   },
-//   box: {
-//     border: "1px solid black",
-//     padding: 8,
-//   },
-//   spreadBox: {
-//     justifyContent: "space-around",
-//     alignItems: "center",
-//   },
-//   rotation: {
-//     transform: [{ rotateX: "360deg" }],
-//   },
-// }));
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "fit-content",
-    border: `2px solid ${theme.palette.divider}`,
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: theme.palette.background.paper,
-    color: theme.palette.text.secondary,
-    "& svg": {
-      // margin: theme.spacing(1.5),
-      height: "50px",
-      width: "50px",
-      marginRight: "20px",
-      marginLeft: "20px",
-    },
-    "& hr": {
-      // margin: theme.spacing(0, 0.5),
-    },
+const menuItems = [
+  {
+    icon: <UndoIcon />,
+    path: "/sketchbox-pages",
+    text: "Undo"
   },
-}));
+  {
+    icon: <MdEdit />,
+    path: "/sketchbox-pages",
+    text: "pencil"
+  },
+];
+
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      width: "fit-content",
+      border: `2px solid ${theme.palette.divider}`,
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: theme.palette.background.paper,
+      color: theme.palette.text.secondary,
+      "& svg": {
+        // margin: theme.spacing(1.5),
+        height: "50px",
+        width: "50px",
+        marginRight: "20px",
+        marginLeft: "20px",
+      },
+      "& hr": {
+        // margin: theme.spacing(0, 0.5),
+      },
+      active: {
+        background: "#f4f4f4"
+      }
+    },
+  }
+});
 export function ToolsBox() {
     const classes = useStyles();
+    const history = useHistory();
+    const location = useLocation();
     return (
       // <Box display="flex" justifyContent="center" pb={10}>
-      <Box display="flex" justifyContent="center" >
-        <Grid container alignItems="center" className={classes.root}>
-          <UndoIcon />
+      <div>
+        {/* <Box display="flex" justifyContent="center"> */}
+        <Box
+          container
+          flexDirection="row"
+          className={classes.root}
+          display="flex"
+          justifyContent="center"
+          alignItems="stretch"
+          padding={1}
+        >
+          <Box>
+            <UndoIcon />
+          </Box>
           <Divider orientation="vertical" flexItem />
-          <MdEdit />
+
+          <Box>
+            <MdEdit />
+          </Box>
+
           <Divider orientation="vertical" flexItem />
-          <Icon icon={eraserIcon} />
+          <Box>
+            <Icon icon={eraserIcon} />
+          </Box>
+
           <Divider orientation="vertical" flexItem />
-          <MdCrop />
+          <Box>
+            {" "}
+            <MdCrop />
+          </Box>
+
           <Divider orientation="vertical" flexItem />
-          <MdColorLens />
+          <Box>
+            {" "}
+            <MdColorLens />
+          </Box>
+
           <Divider orientation="vertical" flexItem />
-          <MdDelete />
+          <Box>
+            <MdDelete />
+          </Box>
+
           <Divider orientation="vertical" flexItem />
-          <RedoIcon />
-        </Grid>
-      </Box>
+          <Box>
+            {" "}
+            <RedoIcon />
+          </Box>
+        </Box>
+        {/* </Box> */}
+
+        {/** List ( Links) */}
+        <List>
+          {menuItems.map((item) => (
+            <ListItem 
+            button 
+            key={item.text} 
+            onClick={() => history.push(item.path)}
+            className={location.pathname === item.path ? classes.active: null} >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
     );
 }
