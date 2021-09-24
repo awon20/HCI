80a99f8494c784f7039432819ded81061888eacb
Armel Wonga <Armel.Wonga@w-hs.de>
2021-09-24 07:30:47 +0200

    add a new page-Comp BoardCard

---
+++ src/pages/CreateSketchBoard/CreateSketchBoard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { BoardCard } from '../../components';
+
+export function CreateSketchBoard() {
+  return (
+    <div>
+      <BoardCard />
+    </div>
+  );
+};
+
