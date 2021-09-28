cf1abb982ab44863ac5882da982d6ced515eb1f4
Armel Wonga <Armel.Wonga@w-hs.de>
2021-09-28 11:05:48 +0200

    add a new theme color switch to this component

--- src/components/BoardLayout/BoardLayout.jsx
+++ src/components/BoardLayout/BoardLayout.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, {  } from "react";
 import clsx from "clsx";
 import { makeStyles } from "@material-ui/core/styles";
 import { Paper, Box } from "@material-ui/core";
 import {
   Drawer as MUIDrawer,
 } from "@material-ui/core/";
-
+// import Typography from "@material-ui/core/Typography";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import AppBar from "@material-ui/core/AppBar";
+// import SwitchUI from "@material-ui/core/Switch";
 import { useHistory } from "react-router";
-
 import {
   StartRecordBoardButton,
   StopRecordBoardButton,
   CameraMicroBox,
   ToolsBox,
   PenDrawing,
-  RecordingButton,
+  RecordingButton
 } from "../../components";
+
 
 // import {  SketchBoardSummary } from "../../pages"
 
@@ -26,32 +30,38 @@ const drawerWidth = 650;
 //     page: <SketchBoardSummary />,
 //     path: "sketchboard-summary",
 //   },
-//   // {
-//   //   page: <BoardClosingDialog />,
-//   //   path: "/sketchbox-pages",
-//   // },
+// {
+//   page: <BoardClosingDialog />,
+//   path: "/sketchbox-pages",
+// },
 // ];
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
   },
-
+  appBar: {
+    zIndex: theme.zIndex.drawer + 0,
+  },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
     whiteSpace: "nowrap",
   },
-
+  drawerContainer: {
+    overflow: "auto",
+  },
   drawerPaper: {
     width: drawerWidth,
   },
-
+  title: {
+    flexGrow: 1,
+  },
   BtnStartSketchRec: {
     margin: theme.spacing(10, "auto"),
   },
   styledComponents: {
-    margin: theme.spacing(5, 'auto'),
+    margin: theme.spacing(5, "auto"),
   },
   contentAlign: {
     margin: theme.spacing(5, "auto"),
@@ -59,15 +69,29 @@ const useStyles = makeStyles((theme) => ({
   BtnStopSketchRec: {
     margin: theme.spacing(10, "auto"),
   },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(0),
+  },
 }));
 export function BoardLayout() {
-// eslint-disable-next-line no-lone-blocks
-{
-  /* Hook functions */
-}
+  // eslint-disable-next-line no-lone-blocks
+  {
+    /* Hook functions */
+  }
   const classes = useStyles();
   const history = useHistory();
+  // const { currentTheme, setTheme } = useContext(CustomThemeContext);
+  // const isDark = Boolean(currentTheme === "dark");
 
+  // const handleThemeChange = (event) => {
+  //   const { checked } = event.target;
+  //   if (checked) {
+  //     setTheme("dark");
+  //   } else {
+  //     setTheme("normal");
+  //   }
+  // };
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -83,6 +107,21 @@ export function BoardLayout() {
         classes={{ paper: classes.drawerPaper }}
         // variant="persistent"
       >
+        {/* Control theme color  */}
+          {/* <Box
+            className={clsx(
+              classes.styledComponents,
+              classes.BtnStartSketchRec
+            )}
+          >
+            <FormControlLabel
+              control={
+                <SwitchUI checked={isDark} onChange={handleThemeChange} />
+              }
+              label="Theme"
+            />
+          </Box> */}
+
         {/* Control SideBar Button */}
         <div>
           <Box
@@ -95,14 +134,12 @@ export function BoardLayout() {
             <StartRecordBoardButton />
           </Box>
         </div>
-
         <div>
           <Box className={clsx(classes.styledComponents, classes.contentAlign)}>
             {/* RecordView for Video from components VideoRecorderComponent  */}
             <RecordingButton />
           </Box>
         </div>
-
         <div>
           <Box className={clsx(classes.styledComponents, classes.contentAlign)}>
             <Paper elevation={1}>
@@ -111,7 +148,6 @@ export function BoardLayout() {
             </Paper>
           </Box>
         </div>
-
         <div>
           <Box className={clsx(classes.styledComponents, classes.contentAlign)}>
             <Paper elevation={1}>
@@ -120,18 +156,15 @@ export function BoardLayout() {
             </Paper>
           </Box>
         </div>
-
         <div>
           <Box
             className={clsx(classes.styledComponents, classes.BtnStopSketchRec)}
           >
             {/* SketchBoard Stop recording from components RecordBoardButton */}
-           
-              <StopRecordBoardButton
-            
-                onClick={handleClick}
-              ></StopRecordBoardButton>
-            
+
+            <StopRecordBoardButton
+              onClick={handleClick}
+            ></StopRecordBoardButton>
           </Box>
         </div>
       </MUIDrawer>
