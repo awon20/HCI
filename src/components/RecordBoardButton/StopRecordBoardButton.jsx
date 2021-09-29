49ddb0cf2607b1d2fe52b2ddbf231fc0a175dbf9
Armel Wonga <Armel.Wonga@w-hs.de>
2021-09-29 17:18:53 +0200

    add history-component to go to the sum-page

--- src/components/RecordBoardButton/StopRecordBoardButton.jsx
+++ src/components/RecordBoardButton/StopRecordBoardButton.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { Box, Button } from "@material-ui/core";
 import Typography from '@material-ui/core/Typography';
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Slide from "@material-ui/core/Slide";
+import { useHistory } from "react-router-dom";
+// import { SketchBoardSummary } from "../../pages/SketchBoardSummary/SketchBoardSummary"
 
 
 
@@ -49,46 +51,52 @@ export const BoardButton = withStyles({
 export function StopRecordBoardButton(props) {
   // add hook for button
   const [open, setOpen] = React.useState(false);
+  // const [done, setDone] = useState(undefined);
+  
+  const history = useHistory();
 
+  const routeChange = () =>{ 
+    let path = `/sketchboard-summary`; 
+    history.push(path);
+  }
   const handleClickOpen = () => {
     setOpen(true);
   };
-  const handleClose = () => {
-    setOpen(false);
-  };
 
   return (
-    <Box display="flex" justifyContent="center">
-      {/* stop Board recording */}
-      <BoardButton
-        variant="contained"
-        color="primary"
-        disableRipple
-        onClick={handleClickOpen}
-      >
-        <Typography
-          variant="button"
-          component="h2"
-          align="center"
-          display="block"
+    <div>
+      <Box display="flex" justifyContent="center">
+        {/* stop Board recording */}
+        <BoardButton
+          variant="contained"
           color="primary"
+          disableRipple
+          onClick={handleClickOpen}
         >
-          Board Abschliessen
-        </Typography>
-      </BoardButton>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-        transitionDuration={500}
-        TransitionComponent={Transition}
-        keepMounted
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Ihre Aufzeichnung wird gespeichert!"}
-        </DialogTitle>
-      </Dialog>
-    </Box>
+          <Typography
+            variant="button"
+            component="h2"
+            align="center"
+            display="block"
+            color="primary"
+          >
+            Board Abschliessen
+          </Typography>
+        </BoardButton>
+      </Box>
+        <Dialog
+          open={open}
+          onClose={routeChange}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          transitionDuration={500}
+          TransitionComponent={Transition}
+          keepMounted
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Ihre Aufzeichnung wird gespeichert!"}
+          </DialogTitle>
+        </Dialog>
+    </div>
   );
 }
