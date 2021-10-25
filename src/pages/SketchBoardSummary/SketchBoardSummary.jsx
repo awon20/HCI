a30dd4b1b1c7ef642aed907e8f1d907b8c8ec10f
Armel Wonga <Armel.Wonga@w-hs.de>
2021-10-25 11:21:38 +0200

    add summary card and page

--- src/pages/SketchBoardSummary/SketchBoardSummary.jsx
+++ src/pages/SketchBoardSummary/SketchBoardSummary.jsx
@@ -1,10 +1,36 @@
-import React from "react"
-import { BoardCard } from "../../components";
+import React from "react";
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from "@material-ui/core/styles";
 
+import { BoardCard } from "../../components";
+import SummeryBoardCard from "./SummaryBoardCard";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    
+  },
+}));
 export function SketchBoardSummary() {
+    const classes = useStyles();
+
   return (
-    <div>
-      <BoardCard />
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={3}
+        direction="row"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={3}>
+          <BoardCard />
+        </Grid>
+        <Grid item xs={3}>
+          <SummeryBoardCard />
+        </Grid>
+      </Grid>
     </div>
   );
 }
